0c6f6c03315f1c9e9b43767887ef7d6a4832935b
TrungNguyen0304 <TrungNguyen15989@gmail.com>
2025-07-07 08:37:26 +0700

    update code

--- backend/controller/QRCodeService.js
+++ backend/controller/QRCodeService.js
@@ -2,8 +2,49 @@ const Client = require("../models/Client");
 const Payment = require("../models/Payment");
 const QRCode = require("qrcode");
 
+// const generateQRCode = (req, res) => {
+//     const { clientKeyId, amount } = req.body;
+
+//     if (typeof amount !== "number" || amount < 0) {
+//         return res.status(400).json({ message: "Invalid amount" })
+//     }
+
+//     Client.findById(clientKeyId)
+//         .then(client => {
+//             if (!client) {
+//                 return res.status(404).json({ message: "Client ID not found" })
+//             }
+//             return Payment.create({
+//                 clientKeyId,
+//                 amount,
+//                 status: "pending",
+//             })
+//                 .then(payment => {
+//                     const qrData = JSON.stringify({
+//                         paymentId: payment._id,
+//                         amount,
+//                         client: client.name,
+//                     });
+//                     return QRCode.toDataURL(qrData)
+//                         .then(qrCodeImage => {
+//                             res.json({
+//                                 paymentId: payment._id,
+//                                 qrCode: qrCodeImage,
+//                                 status: payment.status,
+//                             })
+//                         })
+//                 })
+//         })
+//         .catch(error => {
+//             console.error("QR code error:", error);
+//             res.status(500).json({ message: "Lỗi tạo QR code." });
+//         });
+// }
+
+
 const generateQRCode = (req, res) => {
-    const { clientKeyId, amount } = req.body;
+    const { amount } = req.body;
+    const {clientKeyId} = req.params;
 
     if (typeof amount !== "number" || amount < 0) {
         return res.status(400).json({ message: "Invalid amount" })
@@ -40,6 +81,8 @@ const generateQRCode = (req, res) => {
             res.status(500).json({ message: "Lỗi tạo QR code." });
         });
 }
+
+
 
 const scanQRCode = (req, res) => {
     const { paymentId } = req.params;
